28077ead56be1303488459d32e558edfa4ea9393
Tomi-js <81442111+Tomi-js@users.noreply.github.com>
2021-09-16 18:20:00 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,16 +1,5 @@
 module.exports = async (client) => {
-    
-client.on("ready", () => {
-        const statusList = [
-            "-help",
-            `Gram dla ${client.users.cache.size} użytkowników`
-        ];
-      
-      setInterval(() => {
-            const index = Math.floor(Math.random() * (statusList.length - 1) +1);
-            client.user.setActivity(statusList[index], {type:  'PLAYING'});
-      }, 3000)
-      
     console.log(`Zalogowano jako ${client.user.username}. Bot gotowy na ${client.guilds.cache.size} serwerach, dla ${client.users.cache.size} użytkowników`);
-      });
+
+    client.user.setActivity(`-help`,{type:  'LISTENING'});
 };
