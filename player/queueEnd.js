c985323752cc8556e1001cc6a5778b29d89033dd
Tomi-js <tommyniewiem@gmail.com>
2021-09-17 21:45:35 +0200

    queue fix

--- player/queueEnd.js
+++ player/queueEnd.js
@@ -2,7 +2,7 @@ module.exports = (client, message, queue) => {
     message.channel.send({
         embed: {
             color: 'none',
-            description: `Muzyka się zatrzymała, ponieważ w kolejka jest pusta! [<@${message.member.user.id}>]`,
+            description: `Muzyka się zatrzymała, ponieważ kolejka jest pusta! [<@${message.member.user.id}>]`,
             footer: { text: 'Bot stworzony przez Tomi#9209' },
             timestamp: new Date(),
         },
