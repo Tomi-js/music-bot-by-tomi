a352592034d77757884a1572f7751593ec15e02e
Tomi-js <tommyniewiem@gmail.com>
2021-09-17 23:26:16 +0200

    a

--- player/searchResults.js
+++ player/searchResults.js
@@ -6,6 +6,9 @@ module.exports = (client, message, query, tracks) => {
             footer: { text: 'Bot stworzony przez Tomi#9209' },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
+            fields: [
+                { name: `Na żądanie: [<@${message.member.user.id}>]`},
+            ],
         },
     });
 };
