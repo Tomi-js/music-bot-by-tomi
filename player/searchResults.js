a85562968b87be92e0e5233ad9b866bc391e7138
Tomi-js <tommyniewiem@gmail.com>
2021-09-17 15:43:31 +0200

    first commit

--- player/searchResults.js
+++ player/searchResults.js
@@ -2,7 +2,7 @@ module.exports = (client, message, query, tracks) => {
     message.channel.send({
         embed: {
             color: 'none',
-            author: { name: `Oto wyniki wyszukiwania dla ${query}` },
+            author: { name: `Wyniki wyszukiwania dla ${query}` },
             footer: { text: 'Bot stworzony przez Tomi#9209' },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
