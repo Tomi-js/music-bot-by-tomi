23720f42b2b7ba14a31241687d41fceae02dca15
Tomi-js <tommyniewiem@gmail.com>
2021-09-17 19:26:51 +0200

    kurwa jebany github

--- player/playlistAdd.js
+++ player/playlistAdd.js
@@ -2,17 +2,9 @@ module.exports = (client, message, queue, playlist) => {
     message.channel.send({
         embed: {
             color: 'none',
-<<<<<<< HEAD
             description: `[${playlist.track.title}](${playlist.track.url}) został dodany do kolejki, (zostało ${playlist.tracks.length} piosenek)! [<@${message.member.user.id}>]`,
-=======
-            description: { text: `[${playlist.track.title}](${playlist.track.uri}) został dodany do kolejki, (zostało ${playlist.tracks.length} piosenek) !`},
->>>>>>> c8a7521baf8444cfd70f20bc0f14fd4e1bf9860c
             footer: { text: 'Bot stworzony przez Tomi#9209' },
             timestamp: new Date(),
         },
     });
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> c8a7521baf8444cfd70f20bc0f14fd4e1bf9860c
--- player/searchResults.js
+++ player/searchResults.js
@@ -2,11 +2,7 @@ module.exports = (client, message, query, tracks) => {
     message.channel.send({
         embed: {
             color: 'none',
-<<<<<<< HEAD
             author: { name: `Wyniki wyszukiwania dla ${query} [<@${message.member.user.id}>]` },
-=======
-            author: { name: `Wyniki wyszukiwania dla ${query}` },
->>>>>>> c8a7521baf8444cfd70f20bc0f14fd4e1bf9860c
             footer: { text: 'Bot stworzony przez Tomi#9209' },
             timestamp: new Date(),
             description: `${tracks.map((t, i) => `**${i + 1}** - ${t.title}`).join('\n')}`,
